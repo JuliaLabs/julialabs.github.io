6608058ae99cb7e9b24ce75b0166548475494e69
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2021-11-05 18:54:06 -0400

    publication section (under construction)

---
+++ publications.md
@@ -0,0 +1,27 @@
+# Publications (under construction)
+
+Research on Julia is carried out at the Julia Lab at MIT and at many universities worldwide.
+If you use Julia in your research, we request citing the following paper:
+
+Julia: A Fresh Approach to Numerical Computing. Jeff Bezanson, Alan Edelman, Stefan Karpinski, Viral B. Shah. (2017) SIAM Review, 59: 65–98. doi: 10.1137/141000671.
+
+
+## Publications for the last five years 
+
+{{show_refs 
+    Drake2021
+    Hall2021
+    EDELMAN20211826
+    Drensky2021
+    Souza2020
+    Chan2019127
+    Besard201929
+    Bezanson201765
+    Edelman20161659
+    Edelman2015
+    Movassagh2015342
+    Edelman2015681
+    Dubbs2015188
+}}
+
+
--- utils.jl
+++ utils.jl
@@ -1,3 +1,31 @@
+using BibTeX
+
+function ref_item(ref, infos)
+    io = IOBuffer()
+
+    author = infos["author"]
+    author_last, author_first = strip.(split(author, ","))
+
+    write(io, "<li id=\"#$ref\">")
+    write(io, """$author_first $author_last, <span style="font-style:italic;">$(infos["title"])</span>, $(infos["year"]).""")
+    write(io, "</li>")
+    return String(take!(io))
+end
+
+
+function hfun_show_refs(refs)
+    _, allrefs = parse_bibtex(read(joinpath("_assets", "julialab.bib"), String))
+    out = IOBuffer()
+    write(out, "<ul>")
+    for ref in refs
+        infos = get(allrefs, ref, nothing)
+        isnothing(infos) && continue
+        write(out, ref_item(ref, infos))
+    end
+    write(out, "</ul>")
+    return String(take!(out))
+end
+
 function hfun_bar(vname)
   val = Meta.parse(vname[1])
   return round(sqrt(val), digits=2)
